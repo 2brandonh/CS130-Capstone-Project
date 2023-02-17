72477da03bcea6a33138d30038826b5b826d08e3
kevantony <kevin.m.antony@gmail.com>
2023-02-16 20:54:21 -0800

    moving reset func

--- intellijob/src/Pages/index.js
+++ intellijob/src/Pages/index.js
@@ -1,10 +1,8 @@
 import Register from './Register';
 import Home from './Home.js';
 import Login from './Login';
-import ResetPassword from './ResetPassword.js';
 export {
     Home,
     Login,
     Register,
-    ResetPassword,
 };
