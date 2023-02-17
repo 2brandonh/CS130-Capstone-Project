2df64912c61bac928c90513f8e9172287cdafb7a
kevantony <kevin.m.antony@gmail.com>
2023-02-16 20:59:14 -0800

    Inserted reset methodology and pages

--- intellijob/src/Pages/index.js
+++ intellijob/src/Pages/index.js
@@ -1,8 +1,10 @@
 import Register from './Register';
 import Home from './Home.js';
 import Login from './Login';
+import ResetPassword from './ResetPassword.js';
 export {
     Home,
     Login,
     Register,
+    ResetPassword,
 };
