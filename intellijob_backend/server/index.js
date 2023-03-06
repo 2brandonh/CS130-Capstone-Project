927f02c114707bd3f4906a5eda2586f88dc93ff8
BrandonHsu <brandonh2@g.ucla.edu>
2023-03-06 14:52:49 -0800

    Created working create job endpoint

--- intellijob_backend/server/index.js
+++ intellijob_backend/server/index.js
@@ -1,6 +1,7 @@
-import express from "express";
-import cors from "cors";
-import request from "request";
+const express = require("express");
+const cors = require("cors");
+const {Jobs, Employers, Jobseekers} = require("./config");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
@@ -12,56 +13,48 @@ app.get("/", (req, res) => {
   res.send("working fine");
 });
 
-import dotenv from "dotenv"; // Add to import list
-
-dotenv.config(); // Configure dotenv to access the env variables
-
 const PORT = process.env.PORT || 8080; // Use this instead of hardcoding it like before
-
-const api_key = process.env.REACT_APP_FIREBASE_API_KEY;
-const auth_domain = process.env.REACT_APP_FIREBASE_AUTH_DOMAIN;
-const project_id = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-const storage_bucket = process.env.REACT_APP_FIREBASE_STORAGE_BUCKET;
-const messaging_sender_id = process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID;
-const app_id = process.env.REACT_APP_FIREBASE_APP_ID;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-app.post('/createJob', (req, res) => {
-  company = req.body.company;
-  position = req.body.position;
-  description = req.body.description;
-  skills = req.body.skills;
-  comp = req.body.comp;
-  user_id = req.body.userID;
+app.post('/createJob', async (req, res) => {
+  console.log(req)
+  const company = req.body.company;
+  const position = req.body.position;
+  const description = req.body.description;
+  const location = req.body.location;
+  const skills = req.body.skills;
+  const comp = req.body.comp;
+  const user_id = req.body.userID;
 
   // TODO any preprocessing??
 
-  // make POST request to jobs collection
-  url = "https://firestore.googleapis.com/v1/projects/" + project_id + "/databases/(default)/documents/?&key=" + api_key;
+  const data = req.body
+  await Jobs.add({data})
+  res.send({msg: "Job added"})
 
-  request.post({ headers: {'content-type' : 'application/json'}, url: url, body: req.body }, 
-    (error, response, body) => {
-       console.log(body);
-    });
+  // request.post({ headers: {'content-type' : 'application/json'}, url: url, body: req.body }, 
+  //   (error, response, body) => {
+  //      console.log(body);
+  //   });
 
-  res.send('Success');
+  // res.send('Success');
 });
 
-app.post('/deleteJob', (req, res) => {
+app.post('/deleteJob', cors(), (req, res) => {
   res.send('Hello World!')
 });
 
-app.get('/fetchBookmarkedJobs', (req, res) => {
+app.get('/fetchBookmarkedJobs', cors(), (req, res) => {
   res.send('TODO')
 });
 
-app.get('/fetchCreatedJobs', (req, res) => {
+app.get('/fetchCreatedJobs', cors(), (req, res) => {
   res.send('TODO')
 });
 
-app.get('/fetchRecommendedJobs', (req, res) => {
+app.get('/fetchRecommendedJobs', cors(), (req, res) => {
   res.send('TODO')
 });
