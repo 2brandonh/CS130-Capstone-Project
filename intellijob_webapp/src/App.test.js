cdfd79617bc818ef070b9d53b1a9d020acde53e8
kevantony <kevin.m.antony@gmail.com>
2023-03-12 14:52:47 -0700

    Update App.test.js

--- intellijob_webapp/src/App.test.js
+++ intellijob_webapp/src/App.test.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Signup from './components/Signup/Signup';
+import UserSavedHome from './components/UserSaved/UserSavedHome';
+import ResumeReview from './components/ResumeReview/ResumeReview';
+import JobseekerHome from './components/Jobseeker/JobseekerHome';
+import EmployerHome from './components/Employer/EmployerHome';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('renders UserSavedHome without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<UserSavedHome />, div);
+});
+
+it('renders Signup without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Signup />, div);
+});
+
+it('renders ResumeReview without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ResumeReview />, div);
+});
+
+it('renders JobseekerHome without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<JobseekerHome />, div);
+});
+
+it('renders EmployerHome without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<EmployerHome />, div);
 });
