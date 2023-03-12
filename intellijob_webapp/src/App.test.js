49268665e2f547985b50eb38eaf9d0770578ed5c
kevantony <kevin.m.antony@gmail.com>
2023-03-12 14:46:25 -0700

    Revert "Update App.test.js"
    This reverts commit 702cc139ffa5e7d354d64eb523807779c6850c50.

--- intellijob_webapp/src/App.test.js
+++ intellijob_webapp/src/App.test.js
@@ -12,7 +12,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
- 
+
 
 // it('renders join now', () => {
 //   render(<App />);
