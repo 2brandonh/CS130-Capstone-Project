9f307ee23af238a587f5c0c949b2b214b91e964a
kevantony <kevin.m.antony@gmail.com>
2023-03-19 21:11:50 -0700

    Update App.test.js

--- intellijob_webapp/src/App.test.js
+++ intellijob_webapp/src/App.test.js
@@ -5,6 +5,7 @@ import Signup from './components/Signup/Signup';
 import UserSavedHome from './components/UserSaved/UserSavedHome';
 import ResumeReview from './components/ResumeReview/ResumeReview';
 import JobseekerHome from './components/Jobseeker/JobseekerHome';
+import JobListing from './components/Jobseeker/JobListing';
 import EmployerHome from './components/Employer/EmployerHome';
 
 import '@testing-library/jest-dom';
@@ -20,7 +21,7 @@ it('Sign in rendered', () => {
   expect(screen.getByText('Sign In')).toBeInTheDocument();
 });
 
-it('Sign in rendered', () => {
+it('Join rendered', () => {
   render(<App />);
   expect(screen.getByText('Join Now')).toBeInTheDocument();
 });
@@ -49,3 +50,46 @@ it('renders EmployerHome without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<EmployerHome />, div);
 });
+
+
+const jobInfo = {
+  position: "engineer",
+  comp: 10000,
+  company: "Meta",
+  recruiter: "Mark",
+  skills: ["work"],
+  description: "Do this job",
+  link: "WorkHere"
+}
+
+it('renders Job Listing based on props', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<JobListing jobInfo={jobInfo} user={null}/>, div);
+});
+
+
+it('Job Listing Prop Use is valid', () => {
+  render(<JobListing jobInfo={jobInfo} user={null}/>);
+  expect(screen.getByText('engineer')).toBeInTheDocument();
+});
+
+it('Job Listing Prop Use is valid', () => {
+  render(<JobListing jobInfo={jobInfo} user={null}/>);
+  expect(screen.getByText('Mark')).toBeInTheDocument();
+});
+
+it('Job Listing Prop Use is valid', () => {
+  render(<JobListing jobInfo={jobInfo} user={null}/>);
+  expect(screen.getByText('Do this job')).toBeInTheDocument();
+});
+
+it('Job Listing Prop Use is valid', () => {
+  render(<JobListing jobInfo={jobInfo} user={null}/>);
+  expect(screen.getByText('Meta')).toBeInTheDocument();
+});
+
+it('Job Listing Prop Use is valid', () => {
+  render(<JobListing jobInfo={jobInfo} user={null}/>);
+  expect(screen.getByText('WorkHere')).toBeInTheDocument();
+});
+
